e2f1aa2455b607ad726efbae9ec5832045df457b
Sutar <nikhilsutar621@gmail.com>
2024-06-09 19:56:55 +0530

    course page is completed
    access of data from csv

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,5 @@
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, request
 import csv
-
-
 
 bp = Blueprint('main', __name__)
 
@@ -23,14 +21,17 @@ def index():
 def about():
     return render_template('about.html')
 
-
-#@bp.route('/toppers')
-#def toppers():
-   # return render_template('toppers.html')
-
-
+@bp.route('/course')
+def course():
+    semester_id = request.args.get('semester_id')
+    courses = read_csv_file('course.csv')
+    
+    online_courses = [course for course in courses if course['semester_id'] == semester_id and course['Course_Type'] == 'Online']
+    offline_courses = [course for course in courses if course['semester_id'] == semester_id and course['Course_Type'] == 'Offline']
+    
+    return render_template('course.html', semester_id=semester_id, online_courses=online_courses, offline_courses=offline_courses)
 
 @bp.route('/toppers')
 def toppers():
-    data = read_csv_file('students.csv')  # Ensure 'toppers.csv' is the correct filename
+    data = read_csv_file('students.csv')  # Ensure 'students.csv' is the correct filename
     return render_template('toppers.html', toppers=data)
